1d39e47dcdea7daf991cc99ef7527bd8d64f6a80
Judy21fei bc2024-4 <yuliana.zimirska@lnu.edu.ua>
2024-10-20 02:12:14 +0300

    Second commit for adding code for launching web-server

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ program
   .requiredOption('-c, --cache <cache>');
 
 program.parse(process.argv);
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Server is running');
+});
+
+const { host, port } = program.opts();
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
+});
+
