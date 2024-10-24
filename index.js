5ee2097af1910f0ba29065a41f1007175eb75d26
Judy21fei bc2024-4 <yuliana.zimirska@lnu.edu.ua>
2024-10-24 04:04:20 +0300

    Added methods

--- index.js
+++ index.js
@@ -15,3 +15,80 @@ const options = program.opts();
 
 const requestListener = async function (req, res) {
   const filePath = path.join(options.cache, req.url + ".jpg");
+  switch(req.method){
+    case 'GET':
+        fs.readFile(filePath)
+        .then(content => {
+          console.log('Картинку взято з кешу');
+          res.setHeader("Content-Type", "image/jpeg");
+          res.writeHead(200);
+          res.end(content);
+        })
+        .catch(async () => {
+          try {
+            const content = await superagent.get(path.join('https://http.cat/' + req.url));
+            await fs.writeFile(filePath, content.body);
+            console.log('Картинку взято з сайту');
+            res.setHeader("Content-Type", "image/jpeg");
+            res.writeHead(200);
+            res.end(content.body);
+          } catch (err) {
+            console.log('Запит закінчився помилкою');
+            console.log(err);
+            res.writeHead(404);
+            res.end();
+          }
+        });
+        break;
+        case 'PUT':
+          const chunks = [];
+      
+          //використовуємо масив якщо дані у запиті великі за розміром і їх не можливо отримати за один раз
+          req.on('data', chunk => {
+              chunks.push(chunk);
+          });
+      
+        
+          req.on('end', async () => {
+            //створюємо картинку з фрагментів
+              const imageBuffer = Buffer.concat(chunks); 
+      
+              fs.writeFile(filePath, imageBuffer)
+              .then(()=>{
+                res.setHeader("Content-Type", "text/plain");
+                  res.writeHead(201);
+                  res.end('Image saved successfully.');
+              })
+              .catch(err=>{
+                console.error('Error saving image:', err);
+                res.writeHead(500);
+                res.end('Error saving image.');
+              })
+          });
+      
+          req.on('error', (err) => {
+              console.error('Request error:', err);
+              res.writeHead(400);
+              res.end('Bad request.');
+          });
+          break;
+          case 'DELETE':
+            fs.unlink(filePath)
+            .then(()=>{
+              res.setHeader("Content-Type", "text/plain");
+                  res.writeHead(200);
+                  res.end('Image deleted successfully.');
+            })
+            .catch(() =>{
+              console.error('No picture in cache:', filePath);
+              res.writeHead(404);
+              res.end();
+            });
+            break;
+            default:
+              console.error('Wrong method');
+              res.writeHead(405);
+              res.end();
+              break;
+        }
+      }
